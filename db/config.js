c99587d96eb0aae2d494a804c8e927b2d00414b8
mathi669 <matias.luarte@hotmail.com>
2022-08-22 23:16:56 -0400

    dotenv config

--- db/config.js
+++ db/config.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 const uri = process.env.MONGO_CNN;
 
 const dbCOnnection = async()=>{
 
     try{
-
+        console.log(uri, typeof uri)
         mongoose.connect( uri );
             //mongoose 6 ya no soporta las opciones ingresadas y vienen por defecto activadas por lo que ya no es 
             //necesario utilizar las siguientes opciones
