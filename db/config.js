bce08d85a9d037ddba36216678c9c33404a20c77
mathi669 <matias.luarte@hotmail.com>
2022-08-22 22:48:09 -0400

    modificacion uri

--- db/config.js
+++ db/config.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const uri = process.env.MONGO_CNN;
+
 const dbCOnnection = async()=>{
 
     try{
 
-        mongoose.connect( process.env.MONGO_CNN);
+        mongoose.connect( uri );
             //mongoose 6 ya no soporta las opciones ingresadas y vienen por defecto activadas por lo que ya no es 
             //necesario utilizar las siguientes opciones
                 // useNewUrlParser: true,
